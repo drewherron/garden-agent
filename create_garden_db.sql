fab863f140f404a2656f72f2c250dd1dc6192315
Drew Herron <dherron@pdx.edu>
2025-02-16 17:17:29 -0800

    Use Planting and Season tables (instead of Logs)

--- create_garden_db.sql
+++ create_garden_db.sql
@@ -1,4 +1,9 @@
--- Create the Plants table
+DROP TABLE IF EXISTS Imports;
+DROP TABLE IF EXISTS Plantings;
+DROP TABLE IF EXISTS Seasons;
+DROP TABLE IF EXISTS Plants;
+
+-- 1. Plants
 CREATE TABLE Plants (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
@@ -10,24 +15,36 @@ CREATE TABLE Plants (
     maturity_days INT
 );
 
--- Create the Logs table
-CREATE TABLE Logs (
+-- 2. Plantings
+CREATE TABLE Plantings (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    plant_id INTEGER REFERENCES Plants(id) ON DELETE CASCADE,
+    plant_id INTEGER NOT NULL REFERENCES Plants(id) ON DELETE CASCADE,
     year INT NOT NULL,
-    season TEXT CHECK (season IN ('Spring', 'Summer', 'Fall', 'Winter')),
-    indoor BOOLEAN NOT NULL DEFAULT 0,
-    planting_date DATE,
+    season TEXT NOT NULL CHECK (
+        season IN ('Winter', 'Spring', 'Summer', 'Fall', 'Unknown')
+    ) DEFAULT 'Unknown',
+    batch_number INT NOT NULL DEFAULT 1,
+    seed_start_date DATE,    -- If this is null, we assume not from seed.
     transplant_date DATE,
+    indoor BOOLEAN NOT NULL DEFAULT 0,
     soil_ph DECIMAL,
     pests TEXT,
+    disease TEXT,
     fertilizer TEXT,
     amendments TEXT,
     source TEXT,
     notes TEXT
 );
 
--- Create the Imports table
+-- 3. Seasons
+CREATE TABLE Seasons (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    year INT NOT NULL,
+    expected_last_frost DATE,
+    actual_last_frost DATE
+);
+
+-- 4. Imports
 CREATE TABLE Imports (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL,
@@ -35,3 +52,4 @@ CREATE TABLE Imports (
     note_content TEXT NOT NULL,
     imported_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+
